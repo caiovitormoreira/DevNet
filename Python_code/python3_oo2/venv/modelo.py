9a1789139fa234d14b1e63911808a4cd313b64cc
caiovitormoreira <caiovitor.moreira@gmail.com>
2020-11-25 22:00:16 -0200

    python: finalizado aula de herança. comecando polimorfismo

--- Python_code/python3_oo2/venv/modelo.py
+++ Python_code/python3_oo2/venv/modelo.py
@@ -1,21 +1,48 @@
-class Filme:
-    def __init__(self, nome, ano, duracao, likes):
-        self.nome = nome
+class Programa: #essa agora é uma classe mae. As outras classes vão herdar os atributos e os metodos
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
 
-class Serie:
-    def __init__(self, nome, ano, temporadas, likes=0):
-        self.nome = nome
-        self.ano = ano
-        self.temporadas = temporadas
-        self.likes = 0
+    @property
+    def likes(self):
+        return self.__likes
 
     def likes_increase(self):
-        self.likes += 1
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self):
+        return self.__nome
+
+class Filme(Programa): #passando o nome da classe mae
+    def __init__(self, nome, ano, duracao): #o contrutor continua trazendo os atributos, mas nao precisa declarar todos
+        super().__init__(nome, ano) #esses 2 ja estao declarados
+        self.duracao = duracao
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas  = temporadas
 
 
-house = Serie("house", 2005, 9)
-hymym = Serie("How I met your Mother", 2003, 8)
-print(f'nome:{hymym.nome} - Ano: {hymym.ano} - Temporadas: {hymym.temporadas} - Likes: {hymym.likes}')
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+vingadores.likes_increase()
+vingadores.likes_increase()
+vingadores.likes_increase()
+
+atlanta.likes_increase()
+atlanta.likes_increase()
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+# house = Serie("house", 2005, 9)
+# chequemate = Filme("cheque mate", 2010, 130)
+# print(f'nome: {house.nome} - Ano: {house.ano} - Temporadas: {house.temporadas} - Likes: {house.likes}')
+# print(f'nome: {chequemate.nome} - Ano: {chequemate.ano} - Temporadas: {chequemate.temporadas} - Likes: {chequemate.likes}')
